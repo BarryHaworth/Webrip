0485d09126c7d15b2b3d80983ffa7f5a42316618
BarryHaworth <barryhaworth@gmail.com>
2019-09-30 16:20:17 +1000

    Added Gunnerkrigg

---
+++ Gunnerkrigg.R
@@ -0,0 +1,35 @@
+# Data Download
+# Download the images from Gunnerkrigg Court
+# Image links of form http://www.gunnerkrigg.com/comics/00001752.jpg
+
+library(rvest)
+library(dplyr)
+
+options(timeout= 4000000)
+
+print("Program started")
+timestamp()
+
+PROJECT_DIR <- "c:/R/Webrip"
+FILE_DIR    <- "c:/R/Webrip/Gunnerkrigg"
+
+start = 1752
+stop  = 2206
+
+
+# for testing
+d = start
+Gunnerkrigg <- paste0('https:///www.gunnerkrigg.com/comics/',formatC(d,7,flag="0"),'.jpg')
+#Reading the HTML code from the website
+# webpage <- read_html(url)
+
+
+#Download the Images
+for (i in start:stop){
+  Gunnerkrigg <- paste0('https://www.gunnerkrigg.com/comics/',formatC(i,7,flag="0"),'.jpg')
+  print(paste("downloading file",Gunnerkrigg))
+  download.file(Gunnerkrigg,
+                paste0(FILE_DIR,"/",formatC(i,7,flag="0"),'.jpg'),
+                quiet=TRUE, mode="wb")
+}
+
