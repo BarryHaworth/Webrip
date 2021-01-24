67934399133ab039000f5cff34bd84cb36f7d03c
BarryHaworth <barryhaworth@gmail.com>
2021-01-24 15:50:19 +1000

    Added Volume details from Gunnerkrigg Wiki

--- Gunnerkrigg.R
+++ Gunnerkrigg.R
@@ -13,9 +13,19 @@ timestamp()
 PROJECT_DIR <- "c:/R/Webrip"
 FILE_DIR    <- "c:/R/Webrip/Gunnerkrigg"
 
+# Volume 1: Chapter  1 - 14 Page    1 -  304
+# Volume 2: Chapter 15 - 22 page  305 -  569
+# Volume 3: Chapter 23 - 31 page  570 -  843
+# Volume 4: Chapter 32 - 41 page  844 - 1177
+# Volume 5: Chapter 42 - 49 page 1178 - 1463
+# Volume 6: Chapter 50 - 59 page 1464 - 1752
+# Volume 7: Chapter 60 - 68 page 1753 - 2062
+# Volume 8: Chapter 69 - 74 page 2063 - 2269
+
 # start = 1752 
-start = 2207 # the next one that I don't have, 19/08/2020.
-start = 2378 # the next one that I don't have, 18/01/2021.
+# start = 2207 # the next one that I don't have, 19/08/2020.
+# start = 2378 # the next one that I don't have, 18/01/2021.
+start = 1 
 stop  = 2397 # End of Chapter 78
 
 
