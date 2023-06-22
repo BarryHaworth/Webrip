9b06933fc0440cf5665dfe24235fd65246be61e0
BarryHaworth <barryhaworth@gmail.com>
2023-06-23 08:32:07 +1000

    MOre Wallpaperscraft

---
+++ wallpaper_subset.R
@@ -0,0 +1,58 @@
+# WallpapersCraft
+# Program to manage wallpater files downloaded from 
+# the https://wallpaperscraft.com/ web site
+# and manage a subset of these files to load onto my table
+
+library(dplyr)
+library(tidyr)
+library(stringr)
+library(data.table)
+
+PROJECT_DIR <- "c:/R/Webrip"
+FULL_DIR    <- "e:/wallpaperscraft/1920x1200"
+SUB_DIR     <- "e:/wallpaperscraft/1920x1200 subset"
+
+# Read the contents of the full directory and the subset directory
+
+full_list <- data.frame(file=list.files(FULL_DIR))
+sub_list  <- data.frame(file=list.files(SUB_DIR))
+
+anti_list <- anti_join(full_list,sub_list) # files not in the subset
+
+#  Identify Keywords in both directories, in particular those not in the subset
+
+full_keys <- full_list %>% separate(file,c("key1","key2","key3","key4","key5","key6","key7","key8","key9","key10"),sep="_")
+full_keys <- cbind(full_list,full_keys)
+full_melt  <- melt(full_keys,id=1,value.name="keyword",na.rm=TRUE) %>% 
+              select(-"variable") %>% filter(keyword != "1920x1200.jpg") %>% arrange(file)
+
+sub_keys  <- sub_list %>% separate(file,c("key1","key2","key3","key4","key5","key6","key7","key8","key9","key10"),sep="_")
+sub_keys  <- cbind(sub_list,sub_keys)
+sub_melt  <- melt(sub_keys,id=1,value.name="keyword",na.rm=TRUE) %>% 
+             select(-"variable") %>% filter(keyword != "1920x1200.jpg") %>% arrange(file)
+
+full_key_count <- full_melt %>% count(keyword) %>% arrange(-n)
+sub_key_count  <- sub_melt  %>% count(keyword) %>% arrange(-n)
+
+compare <- full_key_count %>% rename(full_n=n) %>% 
+           full_join(sub_key_count %>% rename(sub_n=n)) %>% 
+           replace(is.na(.), 0) %>%
+           mutate(sub_pct=sub_n/full_n) %>%
+           filter(full_n>1)  
+
+# Identify a list of keywords to copy.
+# There are 92838 files in total, and so far I have 13724 (4.2 GB)
+# Want to increase to 10GB => 32,000 files (another ~19,000)
+
+# Partial
+compare %>% filter(full_n>9 & sub_pct<1 & sub_pct>0.5)
+
+# Given a list of keywords, copy files from full to subset
+#keywords <- c("trees","tree","forest","mountains","flowers","bouquet","sky","sunset")
+keywords <- c("stars","planet","kaleidoscope","planets","satellite","eclipse","asteroids")
+
+new_files <- full_melt %>% filter(keyword %in% keywords) %>% select(file) %>% unique() %>% anti_join(sub_list)
+
+new_list <- list.files(FULL_DIR)[list.files(FULL_DIR) %in% new_files$file]
+
+file.copy(from=paste0(FULL_DIR,"/",new_list),to=paste0(SUB_DIR,"/"))
--- wallpaperscraft.R
+++ wallpaperscraft.R
@@ -11,14 +11,15 @@ library(xml2)
 library(stringr)
 
 PROJECT_DIR <- "c:/R/Webrip"
-FILE_DIR    <- "c:/R/Webrip/wallpaperscraft"
+#FILE_DIR    <- "c:/R/Webrip/wallpaperscraft"
+FILE_DIR    <- "e:/wallpaperscraft"
 dir.create(FILE_DIR,showWarnings = FALSE)
 
 # Stage 1: Page through the list
 # Pages of type: https://wallpaperscraft.com/all/page2
 
 start <- 1
-stop  <- 9175
+stop  <- 9298 # As at 17/06/2023
 
 # Test URL
 url <- 'https://wallpaperscraft.com/all/page2'
@@ -46,20 +47,44 @@ for (i in start:stop){
 wallpaperscraft <- wallpaperscraft %>% unique()
 save(wallpaperscraft,file=paste0(PROJECT_DIR,"/wallpaperscraft.RData"))
 
+# Image Keywords
+# Identify the three keywords for each image and prioritise 
+
+keywords <- data.frame()
+for (i in 1:nrow(wallpaperscraft)){
+  image_name <- strsplit(wallpaperscraft$thumbnail[i],'[/]')
+  image_name <- image_name[[1]][length(image_name[[1]])]
+  keys <- strsplit(image_name,"_")[[1]]
+  for (j in 1:(length(keys)-2)){
+    keyword <- keys[j]
+    #print(paste("Image",i,"Keyword",j,keyword))
+    keywords<- rbind(keywords,data.frame(image_name,keyword))
+  }
+}
+keywords <- keywords %>% unique()
+save(keywords,file=paste0(PROJECT_DIR,"/keywords.RData"))
+
+key_list <- keywords %>% select(keyword) %>% group_by(keyword) %>% mutate(n=n()) %>% unique()
+
+
+#Resolution
+# res <- "1080x1920"  # Phone Resolution
+res <- "1920x1200"  # tablet resolution
+
 # Download the files
 for (i in 1:nrow(wallpaperscraft)){
-  thumbnail <- wallpaperscraft$image_name[i]
+  thumbnail <- wallpaperscraft$thumbnail[i]
   # Get the address for the resolution we want
-  phone_res <- paste0(strtrim(wallpaperscraft$thumbnail[i],nchar(wallpaperscraft$thumbnail[i])-11),"1080x1920.jpg")  
-  image_name <- strsplit(phone_res,'[/]')
+  image_res <- paste0(strtrim(wallpaperscraft$thumbnail[i],nchar(wallpaperscraft$thumbnail[i])-11),res,".jpg")  
+  image_name <- strsplit(image_res,'[/]')
   image_name <- image_name[[1]][length(image_name[[1]])]
   
-  if (file.exists(paste0(FILE_DIR,"/",image_name))){
+  if (file.exists(paste0(FILE_DIR,"/",res,"/",image_name))){
     #print(paste("File",paste0(FILE_DIR,"/",image_name),"Already Exists"))
   } else{
     print(paste("downloading file",i,"of",nrow(wallpaperscraft),image_name))
-    tryCatch({download.file(phone_res,
-                  paste0(FILE_DIR,"/",image_name),
+    tryCatch({download.file(image_res,
+                  paste0(FILE_DIR,"/",res,"/",image_name),
                   quiet=TRUE, mode="wb")},error=function(e){print('Remote File Does not Exist')})
   }
 }
