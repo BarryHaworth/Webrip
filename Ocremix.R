7c255db63b1ac1b33f552d5eb5435dbc816553d0
BarryHaworth <barryhaworth@gmail.com>
2023-09-22 23:14:51 +1000

    More minor changes

--- Ocremix.R
+++ Ocremix.R
@@ -24,7 +24,8 @@ FILE_DIR    <- "c:/R/Webrip/OCRemix"
 #start = 4295 # the next one that I don't have, 08/12/2021
 #stop  = 4307 # Latest file as at 08/12/2021
 start = 4308 # the next one that I don't have, 29/05/2022
-stop  = 4346 # Latest file as at 08/12/2021
+#stop  = 4346 # Latest file as at 08/12/2021
+stop  = 4486 # Latest file as at 22/09/2023
 
 # for testing
 d = start
@@ -40,12 +41,16 @@ name <- word(link,-1,sep='/')
 for (i in start:stop){
   url <- paste0('https://ocremix.org/remix/OCR',formatC(i,4,flag="0"))
   #Reading the HTML code from the website
-  webpage  <- read_html(url)
+  tryCatch({webpage  <- read_html(url)},error=function(e){print('Remote File Does not Exist')})
   mp3      <- html_nodes(webpage,'li') 
   mp3_link <- mp3[grep("blueblue",mp3)]
   link <- xml_attrs(xml_child(mp3_link, 1))[["href"]]
   name <- word(link,-1,sep='/')
-  print(paste("Downloading file number",i,name))
-  download.file(link,paste0(FILE_DIR,"/",name),quiet=TRUE, mode="wb")
+  if (file.exists(paste0(FILE_DIR,"/",name))){
+    print(paste("File number",i,paste0(FILE_DIR,"/",name),"Already Exists"))
+    } else {
+      print(paste("Downloading file number",i,name))
+      download.file(link,paste0(FILE_DIR,"/",name),quiet=TRUE, mode="wb")
+      }
 }
 
