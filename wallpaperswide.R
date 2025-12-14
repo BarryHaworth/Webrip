71b61403d83e0d801df51b77e6f788383064f669
BarryHaworth <barryhaworth@gmail.com>
2025-12-15 07:21:44 +1000

    Corrections to Wallpaers Wide

--- wallpaperswide.R
+++ wallpaperswide.R
@@ -20,7 +20,11 @@ dir.create(FILE_DIR,showWarnings = FALSE)
 # Test URL
 url <- 'https://wallpaperswide.com/page/2'
 url <- 'https://wallpaperswide.com/3840x1080-wallpapers-r/page/2' # select the resolution wanted
+url <- 'https://wallpaperswide.com/1080x2400-wallpapers-r/page/2'
 
+# Set D/L Resolution
+res <- "3840x1080"   # Dual Screen Resolution
+res <- "1080x2400"   # Oppo a96 Resolution
 
 rip_url <- function(url){
   webpage  <- read_html(url)
@@ -29,24 +33,26 @@ rip_url <- function(url){
   return(data.frame(thumbnail=thumbs))
 }
 
-
 #  Get list of image pages from Start to Stop
 
 start <- 1
 #stop  <- 10
-stop  <- 840 # As at 12/12/2025 
+stop  <- 840 # for dual screen As at 12/12/2025 
+stop  <- 126 # Total for A96 
 
-if (file.exists(paste0(PROJECT_DIR,"/wallpaperswide.RData"))){
-  load(paste0(PROJECT_DIR,"/wallpaperswide.RData"))  # Load the data file if it exists
+if (file.exists(paste0(PROJECT_DIR,"/wallpaperswide-",res,".RData"))){
+  load(paste0(PROJECT_DIR,"/wallpaperswide-",res,".RData"))  # Load the data file if it exists
 } else {
-  wallpaperswide <- rip_url('https://wallpaperswide.com/3840x1080-wallpapers-r/page/1')  # Initialise with  first page
+#  wallpaperswide <- rip_url('https://wallpaperswide.com/3840x1080-wallpapers-r/page/1')  # Initialise with  first page
+  wallpaperswide <- rip_url(paste0('https://wallpaperswide.com/',res,'-wallpapers-r/page/1'))  # Initialise with  first page
 }
 
 cume <- 0
 
 for (i in stop:start){
   before <- nrow(wallpaperswide)
-  url <- paste0('https://wallpaperswide.com/page/',i)
+#  url <- paste0('https://wallpaperswide.com/page/',i)
+  url <- paste0('https://wallpaperswide.com/',res,'-wallpapers-r/page/',i)
   cat(paste("Ripping page",i,"url",url))
   thumbs <- tryCatch({rip_url(url)},error=function(e){}) 
   wallpaperswide <- rbind(wallpaperswide,thumbs) %>% unique() %>% arrange(thumbnail)
@@ -56,11 +62,7 @@ for (i in stop:start){
   cat(paste(" Added",added,"images, cume",cume,'\n'))
 }
 
-save(wallpaperswide,file=paste0(PROJECT_DIR,"/wallpaperswide.RData"))
-
-
-# Set D/L Resolution
-res <- "3840x1080"   # Dual Screen Resolution
+save(wallpaperswide,file=paste0(PROJECT_DIR,"/wallpaperswide-",res,".RData"))
 
 # Filter list for the desired resolution?
 
@@ -70,9 +72,7 @@ for (i in 1:nrow(wallpaperswide)){
   dir.create(paste0(FILE_DIR,"/",res),showWarnings = FALSE)
   thumbnail <- wallpaperswide$thumbnail[i]
   # Get the address for the resolution we want
-  #image_res <- paste0(strtrim(wall_filtered$thumbnail[i],nchar(wall_filtered$thumbnail[i])-11),res,".jpg")  
-  image_res  <- thumbnail
-  image_name <- strsplit(image_res,'[/]')[[1]][5]
+  image_name <- strsplit(thumbnail,'[/]')[[1]][5]
   image_name <- sub('t1',res,image_name)
   image_res  <- paste0('https://wallpaperswide.com/download/',image_name)
   
@@ -86,3 +86,12 @@ for (i in 1:nrow(wallpaperswide)){
     cat('\n')
   }
 }
+
+# Purge the Downloaded files for those that are too small
+
+filelist <- list.files(path = paste0(FILE_DIR,"/",res), full.names = TRUE )
+summary(file.size(filelist))
+hist(file.size(filelist))
+
+smallfiles <- filelist[file.size(filelist) < 120000]
+# file.remove(smallfiles)
